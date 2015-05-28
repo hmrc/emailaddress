22c0d0295a9bda9cbbc859b2913bdcaad357bbd8
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-28 14:17:00 +0100

    MDTP-32 Add sbt-auto-build and sbt-git-versioning

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -1,5 +1,7 @@
 import sbt._
 import Keys._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object HmrcBuild extends Build {
 
@@ -9,26 +11,18 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
 
   val appName = "emailaddress"
-  val appVersion = "0.5.0-SNAPSHOT"
 
   lazy val emailaddress = Project(appName, file("."))
-    .settings(version := appVersion)
-    .settings(scalaSettings: _*)
-    .settings(defaultSettings(): _*)
+    .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning)
     .settings(
       targetJvm := "jvm-1.7",
-      shellPrompt := ShellPrompt(appVersion),
       libraryDependencies ++= AppDependencies(),
       resolvers := Seq(
-        Opts.resolver.sonatypeReleases,
-        Opts.resolver.sonatypeSnapshots,
-        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/",
-        "typesafe-snapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+        Resolver.bintrayRepo("hmrc", "releases"),
+        "typesafe-releases" at "http://repo.typesafe.com/typesafe/releases/"
       ),
       crossScalaVersions := Seq("2.11.5")
     )
-    .settings(SbtBuildInfo(): _*)
-    .settings(POMMetadata())
 
 }
 
@@ -55,31 +49,3 @@ private object AppDependencies {
 
   def apply() = compile ++ Test()
 }
-
-
-object POMMetadata {
-
-  def apply() = {
-    pomExtra :=
-      <url>https://www.gov.uk/government/organisations/hm-revenue-customs</url>
-      <licenses>
-        <license>
-          <name>Apache 2</name>
-          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
-        </license>
-      </licenses>
-      <scm>
-        <connection>scm:git@github.com:hmrc/emailaddress.git</connection>
-        <developerConnection>scm:git@github.com:hmrc/emailaddress.git</developerConnection>
-        <url>git@github.com:hmrc/emailaddress.git</url>
-      </scm>
-      <developers>
-        <developer>
-          <id>howyp</id>
-          <name>Howard Perrin</name>
-          <url>http://www.zuhlke.co.uk/</url>
-        </developer>
-      </developers>
-  }
-}
-
