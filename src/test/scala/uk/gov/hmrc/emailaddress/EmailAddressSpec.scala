99bf081b213aa9376e1bf9d32ac9c8f7c2654c96
muralishub <muralispost@gmail.com>
2020-07-16 08:37:22 +0100

    DC-2648: Roll back to what we have in version 3.3.0 as 3.4.0 had some issues

--- src/test/scala/uk/gov/hmrc/emailaddress/EmailAddressSpec.scala
+++ src/test/scala/uk/gov/hmrc/emailaddress/EmailAddressSpec.scala
@@ -27,57 +27,6 @@ class EmailAddressSpec extends WordSpec with Matchers with PropertyChecks with E
       forAll(validEmailAddresses()) { address =>
         EmailAddress(address).value should be(address)
       }
-      validEmailAddressStandardList.foreach{ address =>
-        EmailAddress(address).value should be(address)
-      }
-    }
-
-    "works for worlds longest active email address" in {
-      EmailAddress("contact-admin-hello-webmaster-info-services" +
-        "-peter-crazy-but-oh-so-ubber-cool-english-alphabet-loverer" +
-        "-abcdefghijklmnopqrstuvwxyz@please-try-to.send-me-an-email-if-" +
-        "you-can-possibly-begin-to-remember-this-coz.this-is-the-longest-email-address" +
-        "-known-to-man-but-to-be-honest.this-is-such-a-stupidly-long-sub-domain" +
-        "-it-could-go-on-forever.pacraig.com").value should be(
-        "contact-admin-hello-webmaster-info-services-peter-crazy-but-oh-so-ubber" +
-        "-cool-english-alphabet-loverer-abcdefghijklmnopqrstuvwxyz@please-try-to.send-me-an-email" +
-        "-if-you-can-possibly-begin-to-remember-this-coz.this-is-the-longest-email-address-known-" +
-        "to-man-but-to-be-honest.this-is-such-a-stupidly-long-sub-domain-it-could-go-on-forever.pacraig.com")
-    }
-
-    "works for more dots in email" in {
-      EmailAddress("example@e.example.co.uk").value should be("example@e.example.co.uk")
-      EmailAddress("ex.am.ple@example.com").value should be("ex.am.ple@example.com")
-    }
-
-    "throw an exception for an email that's not considered standard" in {
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("plainaddress")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("#@%^%#$@#$@#.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("Joe Smith <email@domain.com>")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email.domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email@domain@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress(".email@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email.@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email..email@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("あいうえお@domain.com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email@domain.com (Joe Smith)")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email@domain..com")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email@domain")}
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("email@-domain.com")}
-    }
-
-    "throw an exception for an email that starts with .(dot)" in {
-      an [IllegalArgumentException] should be thrownBy {EmailAddress(".example.e@test.com")}
-    }
-
-    "throw an exception for an email that ends with .(dot)" in  {
-      an [IllegalArgumentException] should be thrownBy {EmailAddress("example@test.co.")}
-    }
-
-    "throw an exception for an email that has ..(2 dots) next to each other" in {
-    an [IllegalArgumentException] should be thrownBy {EmailAddress("example..e@test.com")}
-    an [IllegalArgumentException] should be thrownBy {EmailAddress("example@test..com")}
     }
 
     "throw an exception for an invalid email" in {
